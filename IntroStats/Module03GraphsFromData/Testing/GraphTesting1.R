797c925e224018aa307662341d2f64d43fd4b43b
Alan Ableson <ableson@queensu.ca>
2015-09-29 16:10:21 -0400

    Automatic update from (SurfaceAbleson)

--- IntroStats/Module03GraphsFromData/Testing/GraphTesting1.R
+++ IntroStats/Module03GraphsFromData/Testing/GraphTesting1.R
@@ -1,2 +1,2 @@
 MyData = read.csv("../../Module03_Datasets/LoblollyPines.csv")
-plot(MyData$TreeAge, MyData$TreeHeight, xlab="Tree Age (yrs)", ylab = "Tree Height (m)")
+plot(MyData$TreeAge, MyData$TreeHeight, xlab='Tree Age (yrs)', ylab = "Tree Height (m)")

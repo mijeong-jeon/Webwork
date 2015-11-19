a856782c586074a29286e3beba29b7614a03513f
Alan Ableson <ableson@mast.queensu.ca>
2015-11-18 21:25:00 -0500

    Automatic update from (X220-Ableson)

--- IntroStats/Module10ANOVADataAnalysis/GrowthHormonesSolutions.R
+++ IntroStats/Module10ANOVADataAnalysis/GrowthHormonesSolutions.R
@@ -1,24 +1,27 @@
-# Growth Hormones
+# Growth Hormones Solutions
+
 MyData = read.csv("../Module10_Datasets/GrowthHormonesA.csv")
-png("GrowthHormonesA.png", width=300,height=300)
 boxplot(MyData$Growth ~ MyData$Treatment)
-dev.off()
+MyFit = lm(Growth ~ Treatment, data = MyData)
+summary(MyFit)
+TukeyHSD(aov(MyFit))
 
 MyData = read.csv("../Module10_Datasets/GrowthHormonesB.csv")
-png("GrowthHormonesB.png", width=300,height=300)
 boxplot(MyData$Growth ~ MyData$Treatment)
-dev.off()
+MyFit = lm(Growth ~ Treatment, data = MyData)
+summary(MyFit)
+TukeyHSD(aov(MyFit))
 
 
 MyData = read.csv("../Module10_Datasets/GrowthHormonesC.csv")
-png("GrowthHormonesC.png", width=300,height=300)
 boxplot(MyData$Growth ~ MyData$Treatment)
-dev.off()
+MyFit = lm(Growth ~ Treatment, data = MyData)
+summary(MyFit)
+TukeyHSD(aov(MyFit))
 
 
 MyData = read.csv("../Module10_Datasets/GrowthHormonesD.csv")
-png("GrowthHormonesD.png", width=300,height=300)
 boxplot(MyData$Growth ~ MyData$Treatment)
-dev.off()
-
-
+MyFit = lm(Growth ~ Treatment, data = MyData)
+summary(MyFit)
+TukeyHSD(aov(MyFit))

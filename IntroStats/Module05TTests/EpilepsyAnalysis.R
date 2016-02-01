1b0588cb7b70203cfef349a3fb53cdecb58e8a2a
Alan Ableson <ableson@mast.queensu.ca>
2016-02-01 13:11:02 -0500

    Automatic update from (popfund)

---
+++ IntroStats/Module05TTests/EpilepsyAnalysis.R
@@ -0,0 +1,10 @@
+# Analysis of the epilepsy data
+# Source: page 412 of Gould & Ryan
+
+# valproate
+ci = c(88, 97)
+n = 18
+tcrit = abs(qt(0.025, n-1))
+xbar = mean(ci)
+se = (max(ci) - xbar)/tcrit
+sd = se * sqrt(n)

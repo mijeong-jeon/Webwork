2cac9d609dbd53786b993f401952d965174678bf
Alan Ableson <ableson@queensu.ca>
2016-02-26 14:59:50 -0500

    Automatic update from (popfund)

--- IntroStats/Module07CorrelationWithR/DepartmentAttitudeTests.R
+++ IntroStats/Module07CorrelationWithR/DepartmentAttitudeTests.R
@@ -4,19 +4,19 @@ MyData = read.csv('../Module07_Datasets/DepartmentAttitudeSurvey.csv')
 pairs(MyData)
 
 #png("attitude1.png", width=400,height=400)
-#plot(MyData$Complaints, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
+plot(MyData$Complaints, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
 #dev.off()
 
 #png("attitude2.png", width=400,height=400)
-#plot(MyData$Privileges, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
+plot(MyData$Privileges, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
 #dev.off()
 
 #png("attitude3.png", width=400,height=400)
-#plot(MyData$Raises, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
+plot(MyData$Raises, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
 #dev.off()
 
 #png("attitude4.png", width=400,height=400)
-#plot(MyData$Advancement, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
+plot(MyData$Advancement, MyData$OverallRating, ylab='Overall Rating', xlab="[variable]", xlim=c(30, 100), ylim=c(30, 100))
 #dev.off()
 
 cor.test(MyData$Complaints, MyData$OverallRating)

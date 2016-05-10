72e190b0341d9e4e8f29de5c782e069587e5a267
Alan Ableson <ableson@queensu.ca>
2016-05-10 11:17:06 -0400

    Automatic update from (DESKTOP-DK9BGGI)

--- MNTC/MATLAB_02_GraphingMultipleGraphs/q_001.m
+++ MNTC/MATLAB_02_GraphingMultipleGraphs/q_001.m
@@ -1,5 +1,5 @@
 % q_001
-
+close all
 xpoints = [-1.5, -0.5, 0.5, 1.5];
 for (i = 1:length(xpoints))
     clf
@@ -16,13 +16,32 @@ for (i = 1:length(xpoints))
     print('-dpng', sprintf('q_001_%d_%d.png', fun, i));
     
     fun = 2;
+    clf
     y = 1./x.^2;
     plot(x, y, 'linewidth', 2);
     hold on;
-    y2 = -2/xpoint^3*(x - xpoint) + 1/xpoint;
+    y2 = -2/xpoint^3*(x - xpoint) + 1/xpoint.^2;
+    plot(x, y2, 'r', 'linewidth', 2);
+    print('-dpng', sprintf('q_001_%d_%d.png', fun, i));
+    
+    fun = 3;
+    clf
+    y = -1./x;
+    plot(x, y, 'linewidth', 2);
+    hold on;
+    y2 = +1/xpoint^2*(x - xpoint) - 1/xpoint;
+    plot(x, y2, 'r', 'linewidth', 2);
+    print('-dpng', sprintf('q_001_%d_%d.png', fun, i));
+    
+    fun = 4;
+    clf
+    y = -1./x.^2;
+    plot(x, y, 'linewidth', 2);
+    hold on;
+    y2 = 2/xpoint^3*(x - xpoint) - 1/xpoint.^2;
     plot(x, y2, 'r', 'linewidth', 2);
     print('-dpng', sprintf('q_001_%d_%d.png', fun, i));
     
     
-    pause(2);
+    %     pause(2);
 end

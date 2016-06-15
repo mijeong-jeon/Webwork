648622bb3235bf7d446e907924bbc446d04994dc
Alan Ableson <ableson@queensu.ca>
2016-06-15 16:05:31 -0400

    Automatic update from (popfund)

--- MNTC/MATLAB_09_TemperatureAndSpringModels/q_002.m
+++ MNTC/MATLAB_09_TemperatureAndSpringModels/q_002.m
@@ -1,9 +1,10 @@
 close all;
 m = 2;
-k = 50;
-c = 2;
+k = 26;
+c = 12;
+om = 3.6;
 
-DE = @(t, w) [w(2); (1/m)*(-k * w(1) - c*w(2))];
+DE = @(t, w) [w(2); (1/m)*(-k * w(1) - c*w(2) + cos(om*t))];
 
 tspan = [0, 30];
 y0 = 12;
@@ -11,4 +12,5 @@ w0 = [y0, 0];
 
 [t, w] = ode45( DE, tspan, w0);
 plot(t, w(:,1));
-fprintf('Period = %.3f\n', 2*pi*sqrt(m/k));
+fprintf('Period = %.3f s\n', 2*pi*sqrt(m/k));
+fprintf('Natural freq = %.3f rad/s\n', sqrt(k/m));
